7b025aa4ceed1238c39ea2ec36f102aeeb0b4c5d
firedragon0314 <56679739+firedragon0314@users.noreply.github.com>
2019-12-10 21:49:14 +0800

    Update 27029 .cpp

--- 27029 .cpp
+++ 27029 .cpp
@@ -8,67 +8,14 @@ int main() {
 
     if(number / 10000000000000 == 34 || number / 10000000000000 == 37){
         cout << "American Express\n";
-
-        int result = 0;
-
-        for (int i = 1; i <= 16; i++){
-            int input = number % 10;
-
-            if (i % 2 == 0){
-                if (2 * input >= 10){
-                    int variable1 = 0;
-                    int variable2 = 2 * input;
-                    for (int j = 1; j <= 2; j++){
-                        variable1 += variable2 % 10;
-                        variable2 /= 10;
-                    }
-                    result += variable1;
-                } else {
-                result += 2 * input;
-                }
-            } else{
-                result += input;
-            }
-            number /= 10;
-        }
-            if (result % 10 == 0){
-            cout << "正確";
-            } else {
-            cout << "錯誤";
-            }
     } else if(number / 100000000000000 == 51 || number / 100000000000000 == 55){
         cout << "MasterCard\n";
-
-        int result = 0;
-
-        for (int i = 1; i <= 16; i++){
-            int input = number % 10;
-
-            if (i % 2 == 0){
-                if (2 * input >= 10){
-                    int variable1 = 0;
-                    int variable2 = 2 * input;
-                    for (int j = 1; j <= 2; j++){
-                        variable1 += variable2 % 10;
-                        variable2 /= 10;
-                    }
-                    result += variable1;
-                } else {
-                result += 2 * input;
-                }
-            } else{
-                result += input;
-            }
-            number /= 10;
-        }
-            if (result % 10 == 0){
-            cout << "正確";
-            } else {
-            cout << "錯誤";
-            }
     } else if(number / 1000000000000 == 4 || number / 1000000000000000 == 4){
         cout << "Visa\n";
-
+    } else{
+        cout << "無效卡號";
+    }
+    if(number / 10000000000000 == 34 || number / 10000000000000 == 37 || number / 100000000000000 == 51 || number / 100000000000000 == 55 || number / 1000000000000 == 4 || number / 1000000000000000 == 4){
         int result = 0;
 
         for (int i = 1; i <= 16; i++){
@@ -84,19 +31,17 @@ int main() {
                     }
                     result += variable1;
                 } else {
-                result += 2 * input;
+                    result += 2 * input;
                 }
             } else{
                 result += input;
             }
-            number /= 10;
+        number /= 10;
         }
             if (result % 10 == 0){
             cout << "正確";
             } else {
             cout << "錯誤";
             }
-    } else{
-        cout << "無效卡號";
     }
 }
